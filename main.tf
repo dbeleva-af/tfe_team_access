066e40dea8e64829cfef138e7eb15fd35860642f
dbeleva-af <dbeleva_af@abv.bg>
2023-10-10 17:20:32 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.49.2"
+    }
+  }
+}
+
+provider "tfe" {
+  # Configuration options
+}
+
+resource "tfe_team" "admin" {
+  name         = "my-admin-team"
+  organization = "diana-viktorova"
+}
+
+resource "tfe_project" "test" {
+  name         = "myproject"
+  organization = "diana-viktorova"
+}
+
+resource "tfe_team_project_access" "admin" {
+  access       = "admin"
+  team_id      = tfe_team.admin.id
+  project_id   = tfe_project.test.id
+
+  project_access {
+    settings = "read"
+    teams    = "admin"
+  }
+}
