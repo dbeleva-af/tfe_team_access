829877af439147353dadc01584bddbaf352565f8
dbeleva-af <dbeleva_af@abv.bg>
2023-10-11 13:20:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,34 +11,18 @@ provider "tfe" {
   # Configuration options
 }
 
-resource "tfe_team" "dev" {
-  name         = "my-dev-team"
-  organization = "my-org-name"
+resource "tfe_team" "admin" {
+  name         = "my-admin-team"
+  organization = "diana-viktorova"
 }
 
 resource "tfe_project" "test" {
   name         = "myproject"
-  organization = "my-org-name"
+  organization = "diana-viktorova"
 }
 
-resource "tfe_team_project_access" "custom" {
-  access       = "custom"
-  team_id      = tfe_team.dev.id
+resource "tfe_team_project_access" "admin" {
+  access       = "admin"
+  team_id      = tfe_team.admin.id
   project_id   = tfe_project.test.id
-
-  project_access {
-    settings = "read"
-    teams    = "none"
-  }
-  workspace_access {
-    state_versions = "write"
-    sentinel_mocks = "none"
-    runs           = "apply"
-    variables      = "write"
-    create         = true
-    locking        = true
-    move           = false
-    delete         = false
-    run_tasks      = false
-  }
 }
